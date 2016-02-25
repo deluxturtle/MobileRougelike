db022bc396a0424ec616891698aaf5dc16508074
Andrew Seba <sebaturtle@gmail.com>
2016-02-25 15:25:21 -0600

    Added in the one finger and two finger gestures

---
+++ OneFinger/Assets/Scripts/OneFingerGesture.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OneFingerGesture : MonoBehaviour {
+
+    public int i_comfort = 3;
+    GameObject objectToMove;
+    private Vector2 v2_previous;
+    private Vector2 v2_current;
+    private float f_touch_delta;
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.touchCount == 1)
+        {
+            //Begin Touch Phase
+            if(Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                v2_previous = Input.GetTouch(0).position;
+            }
+
+            //On touch End.
+            if(Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                v2_current = Input.GetTouch(0).position;
+                f_touch_delta = v2_current.magnitude - v2_previous.magnitude;
+
+                //If the magnitude of swipe is greater than 3
+                if (Mathf.Abs(f_touch_delta) > i_comfort)
+                {
+                    Debug.Log("Swipe");
+                    //If swiping left or down
+                    if(f_touch_delta < 0)
+                    {
+                        //if x is greater to the left than going down then we are going left
+                        if (Mathf.Abs(v2_current.x - v2_previous.x) > Mathf.Abs(v2_current.y - v2_previous.y))
+                        {
+                            print("Left");
+                        }
+                        else
+                        {
+                            print("Down");
+                        }
+                    }
+                    //else we are swiping right or up.
+                    else
+                    {
+                        if(Mathf.Abs(v2_current.x - v2_previous.x) > Mathf.Abs(v2_current.y - v2_previous.y))
+                        {
+                            print("Right");
+                        }
+                        else
+                        {
+                            print("Up");
+                        }
+                    }
+                }
+                else
+                {
+                    //Single Tap
+                    if (Input.GetTouch(0).tapCount == 1)
+                    {
+
+                        RaycastHit hit;
+                        Ray ray;
+                        ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+
+                        if (Physics.Raycast(ray, out hit, 100.0f))
+                        {
+
+                            if (objectToMove != null && hit.transform.name != objectToMove.transform.name)
+                            {
+                                //Stop particle.
+                                objectToMove.transform.GetChild(0).gameObject.SetActive(false);
+                            }
+                            objectToMove = hit.transform.gameObject;
+
+                            objectToMove.transform.GetChild(0).gameObject.SetActive(true);
+
+                        }
+                    }
+                    //Double Tap
+                    else if (Input.GetTouch(0).tapCount == 2)
+                    {
+                        if (objectToMove != null)
+                        {
+                            Vector3 pos;
+                            pos.x = Input.GetTouch(0).position.x;
+                            pos.y = Input.GetTouch(0).position.y;
+
+                            pos.z = Mathf.Abs(Camera.main.transform.position.z - objectToMove.transform.position.z);
+
+                            objectToMove.transform.position = Camera.main.ScreenToWorldPoint(pos);
+                        }
+                    }
+                }
+
+            }
+
+        }
+    }
+}
---
+++ TwoFinger/Assets/Scripts/ZoomRotate.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// @Author: Andrew Seba
+/// </summary>
+public class ZoomRotate : MonoBehaviour {
+    
+    public GameObject cubeToRotate;
+    public float minSwipeDistance = 2;
+    public Camera myCamera;
+    public float zoomAmount = 5;
+    public float rotateAmount = 1;
+
+    private Vector2 v2_current;
+    private Vector2 v2_previous;
+    private float touchMagnitude;
+    private float angle;
+	
+
+	void Update ()
+    {
+	    if(Input.touchCount == 2 &&
+            Input.GetTouch(0).phase == TouchPhase.Moved &&
+            Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            v2_current = Input.GetTouch(0).position - Input.GetTouch(1).position;
+            v2_previous = (Input.GetTouch(0).position - Input.GetTouch(0).deltaPosition) -
+                (Input.GetTouch(1).position - Input.GetTouch(1).deltaPosition);
+
+            //Calculate the distance between the two fingers.
+            touchMagnitude = v2_current.magnitude - v2_previous.magnitude;
+            //Grab the angle.
+            angle = Vector2.Angle(v2_previous, v2_current);
+
+            if(angle > 0.1)
+            {
+                Debug.Log(Vector3.Cross(v2_current, v2_previous));
+                if(Vector3.Cross(v2_current, v2_previous).z < 0.0f)
+                {
+                    Debug.Log("Counter Clockwise");
+                    cubeToRotate.transform.Rotate(Vector3.up, angle * -1 * rotateAmount);
+                }
+                else
+                {
+                    Debug.Log("Clockwise");
+                    cubeToRotate.transform.Rotate(Vector3.up, angle * rotateAmount);
+                }
+            }
+
+            //Zoom gesture
+            if(Mathf.Abs(touchMagnitude) > minSwipeDistance)
+            {
+                //print("Zoom detected");
+                if(touchMagnitude > 0)
+                {
+                    //zoom in
+                    myCamera.fieldOfView = Mathf.Clamp( Mathf.Lerp(myCamera.fieldOfView, myCamera.fieldOfView - Mathf.Abs(touchMagnitude) * zoomAmount, Time.deltaTime), 10,70);
+
+                    Debug.Log("Zoom In");
+                }
+                else
+                {
+                    //zoom out
+                    myCamera.fieldOfView = Mathf.Clamp(Mathf.Lerp(myCamera.fieldOfView, myCamera.fieldOfView + Mathf.Abs(touchMagnitude) * zoomAmount, Time.deltaTime), 10, 70);
+
+                    Debug.Log("Zoom Out");
+                }
+            }
+        }
+	}
+}
