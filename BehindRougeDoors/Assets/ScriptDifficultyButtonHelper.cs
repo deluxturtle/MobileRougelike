9e15362c8b1db4b2b98219f9500d5f4dfaf29c33
Andrew Seba <sebaturtle@gmail.com>
2016-04-05 15:42:52 -0500

    Difficulty buttons work.

---
+++ BehindRougeDoors/Assets/ScriptDifficultyButtonHelper.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Author: Andrew Seba
+/// Description: handles some of the button functionaly and sprite changing.
+/// </summary>
+public class ScriptDifficultyButtonHelper : MonoBehaviour {
+
+    [Header("Difficulty Buttons")]
+    public GameObject easyButton;
+    public GameObject mediumButton;
+    public GameObject hardButton;
+
+    [Header("Button Graphics")]
+    [Tooltip("The image you want to show on the normal buttons.")]
+    public Sprite normalButton;
+    [Tooltip("The image you want to show on the selected buttons.")]
+    public Sprite selectedButton;
+
+    private ScriptGameManager gameManager;
+
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<ScriptGameManager>();
+        UpdateDifficultySelection();
+    }
+
+    void OnEnable()
+    {
+        if(gameManager == null)
+            gameManager = GameObject.Find("GameManager").GetComponent<ScriptGameManager>();
+
+        UpdateDifficultySelection();
+    }
+
+    /// <summary>
+    /// Changes the sprites on the buttons to highlight the selected difficulty
+    /// </summary>
+    public void UpdateDifficultySelection()
+    {
+        switch (gameManager.currentDifficulty)
+        {
+            case Difficutly.EASY:
+                easyButton.GetComponent<Image>().sprite = selectedButton;
+                mediumButton.GetComponent<Image>().sprite = normalButton;
+                hardButton.GetComponent<Image>().sprite = normalButton;
+                break;
+            case Difficutly.MEDIUM:
+                easyButton.GetComponent<Image>().sprite = normalButton;
+                mediumButton.GetComponent<Image>().sprite = selectedButton;
+                hardButton.GetComponent<Image>().sprite = normalButton;
+                break;
+            case Difficutly.HARD:
+                easyButton.GetComponent<Image>().sprite = normalButton;
+                mediumButton.GetComponent<Image>().sprite = normalButton;
+                hardButton.GetComponent<Image>().sprite = selectedButton;
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Button hook to change the difficulty to easy.
+    /// </summary>
+    public void _SetDifficultyEasy()
+    {
+        gameManager.ChangeDifficutly(Difficutly.EASY);
+        UpdateDifficultySelection();
+    }
+
+    /// <summary>
+    /// Button hook to change the difficulty to easy.
+    /// </summary>
+    public void _SetDifficultyMedium()
+    {
+        gameManager.ChangeDifficutly(Difficutly.MEDIUM);
+        UpdateDifficultySelection();
+    }
+
+    /// <summary>
+    /// Button hook to change the difficulty to easy.
+    /// </summary>
+    public void _SetDifficultyHard()
+    {
+        gameManager.ChangeDifficutly(Difficutly.HARD);
+        UpdateDifficultySelection();
+    }
+
+}
---
+++ BehindRougeDoors/Assets/ScriptGameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// What difficulty the game is initially set at.
+/// </summary>
+public enum Difficutly
+{
+    EASY,
+    MEDIUM,
+    HARD
+}
+
+/// <summary>
+/// @Author: Andrew Seba
+/// @Description: Controls difficulty and level scaling between levels when
+/// traversing through the game.
+/// </summary>
+public class ScriptGameManager : MonoBehaviour
+{
+    public Difficutly currentDifficulty = Difficutly.MEDIUM;
+	
+    public void ChangeDifficutly(Difficutly pDifficulty)
+    {
+        currentDifficulty = pDifficulty;
+    }
+
+}
