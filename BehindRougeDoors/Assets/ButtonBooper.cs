1975bcabee3122fe702603136c4c9563e1046d88
Andrew Seba <sebaturtle@gmail.com>
2016-04-05 16:53:36 -0500

    Button Booper

---
+++ BehindRougeDoors/Assets/ButtonBooper.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonBooper : MonoBehaviour {
+
+
+    public AudioClip boop;
+    AudioSource source;
+
+    void Start()
+    {
+        if(source == null)
+        {
+            source = gameObject.AddComponent<AudioSource>();
+        }
+        source.clip = boop;
+
+        foreach(Button button in GameObject.FindObjectsOfType<Button>())
+        {
+            button.onClick.AddListener(PlayBoop);
+        }
+    }
+
+    void PlayBoop()
+    {
+        source.Play();
+    }
+}
